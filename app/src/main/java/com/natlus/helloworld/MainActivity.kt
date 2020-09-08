223ec4225ce109309b122dcf7c3e05ea4b40104d
Sultan Achmad <sultangendut@gmail.com>
2020-09-08 13:51:10 +0700

    add log

--- app/src/main/java/com/natlus/helloworld/MainActivity.kt
+++ app/src/main/java/com/natlus/helloworld/MainActivity.kt
@@ -2,10 +2,17 @@ package com.natlus.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "Info Main Activity")
+        Log.d(TAG, "Debug Log")
+        Log.e(TAG, "Error log")
+        Log.w(TAG,"Warning log")
     }
 }
